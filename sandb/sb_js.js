3fcbda694fb82e06a80a84836c11e043ebe147a0
VeronDiego <misaponia@hotmail.com>
2022-02-11 18:00:39 -0300

    xD
    xD

---
+++ sandb/sb_js.js
@@ -0,0 +1,11 @@
+/* comentario*/
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function escribir() {
+    const x = '<div class = "container-fluid mt-5 border-2" > <div class = "card"><div class = "card-body p-0"><img src = "../images/otros/web-en-construcción.jpg"class = "img-thumbnail float-start"alt = "foto"width = "304" height = "236"><h4 class = "card-title" > Este sitio esta en construccion. </h4><p class = "card-text" >En poco estará funcionando.</p> </div> </div> </div>';
+    document.write(x);
+}
